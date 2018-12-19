fe081bc978572766dd1494290a8000827c377d0a
Drake Rentrop <drakerentrop@gmail.com>
2018-12-19 15:12:24 +0000

    Done.

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,18 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+self.end_with?("?")
   end
 
   def exclamation?
-
+self.end_with?("!")
   end
 
   def count_sentences
-
+self.split(/[.!?]/).map{|x| !(x.match(/\w+/).nil?)}.reject{|x| x == false}.size
   end
 end
